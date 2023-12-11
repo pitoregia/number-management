f2af51ba3d82f0a16ce28bfbe088c7dacf4269cd
pitoregia <mahesadanuarta@gmail.com>
2023-12-11 16:39:05 +0700

    db changed for multilevel login

--- database/dbnumber.sql
+++ database/dbnumber.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Dec 07, 2023 at 10:47 AM
+-- Generation Time: Dec 11, 2023 at 10:38 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.1.12
 
@@ -20,6 +20,63 @@ SET time_zone = "+00:00";
 --
 -- Database: `dbnumber`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `permission`
+--
+
+CREATE TABLE `permission` (
+  `permission_id` varchar(15) NOT NULL,
+  `permission_name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `permission`
+--
+
+INSERT INTO `permission` (`permission_id`, `permission_name`) VALUES
+('edit_number', 'Edit Number'),
+('edit_user', 'Edit User');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `role`
+--
+
+CREATE TABLE `role` (
+  `role_id` int(11) NOT NULL,
+  `role_name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `role`
+--
+
+INSERT INTO `role` (`role_id`, `role_name`) VALUES
+(1, 'ADMIN'),
+(2, 'USER');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `role_permission`
+--
+
+CREATE TABLE `role_permission` (
+  `role_id` int(11) NOT NULL,
+  `permission_id` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `role_permission`
+--
+
+INSERT INTO `role_permission` (`role_id`, `permission_id`) VALUES
+(1, 'edit_number'),
+(1, 'edit_user');
 
 -- --------------------------------------------------------
 
@@ -42,10 +99,10 @@ CREATE TABLE `tnumber` (
 --
 
 INSERT INTO `tnumber` (`id`, `nomor_telp`, `deskripsi`, `status`, `tanggal_aktif`, `tanggal_expired`, `tanggal_simpan`) VALUES
-(15, '088813276642', 'nox', 'HIDUP', '2023-12-31', '2023-12-31', '2023-12-07 09:29:15'),
-(16, '081277362209', 'Deskripsi', 'TENGGANG', '2023-12-07', '2023-12-07', '2023-12-07 09:33:44'),
-(17, '85733947500', 'Direct testing', 'MATI', '2023-12-07', '2023-12-07', '2023-12-07 07:11:28'),
-(18, '085177888832', 'Aku Joko', 'HIDUP', '2023-12-07', '2023-12-07', '2023-12-07 08:19:23'),
+(15, '088813276642', 'noxes', 'HIDUP', '2024-01-31', '2024-02-29', '2023-12-08 08:44:00'),
+(16, '081277362209', 'Deskripsi', 'TENGGANG', '2023-12-07', '2023-12-07', '2023-12-07 09:48:44'),
+(17, '85733947500', 'Direct testing', 'HIDUP', '2023-12-31', '2024-01-31', '2023-12-08 08:44:17'),
+(18, '085177888832', 'Aku Joko', 'TENGGANG', '2023-12-07', '2023-12-07', '2023-12-08 07:49:27'),
 (19, '023774633291', 'Testing 4', 'MATI', '2023-12-31', '2023-12-31', '2023-12-07 08:21:25');
 
 -- --------------------------------------------------------
@@ -59,22 +116,42 @@ CREATE TABLE `user` (
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `role` varchar(50) NOT NULL,
-  `name` varchar(50) NOT NULL
+  `name` varchar(50) NOT NULL,
+  `role_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id`, `username`, `password`, `role`, `name`) VALUES
-(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'admin', 'Admin'),
-(2, 'user', 'ee11cbb19052e40b07aac0ca060c23ee', 'user', 'User'),
-(3, 'esa', '80ad0b9fa48a74fe86a9c8ee665d96bb', 'admin', 'Mahesa Danuarta'),
-(4, 'joko', '9ba0009aa81e794e628a04b51eaf7d7f', 'user', 'Raihan Revi');
+INSERT INTO `user` (`id`, `username`, `password`, `role`, `name`, `role_id`) VALUES
+(1, 'admin', 'd41d8cd98f00b204e9800998ecf8427e', 'ADMIN', 'Admin', 1),
+(2, 'user', 'ee11cbb19052e40b07aac0ca060c23ee', 'USER', 'User', 2),
+(3, 'esa', '80ad0b9fa48a74fe86a9c8ee665d96bb', 'ADMIN', 'Mahesa Danuarta', 1),
+(4, 'joko', '9ba0009aa81e794e628a04b51eaf7d7f', 'USER', 'Raihan Revi', 2);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `permission`
+--
+ALTER TABLE `permission`
+  ADD PRIMARY KEY (`permission_id`);
+
+--
+-- Indexes for table `role`
+--
+ALTER TABLE `role`
+  ADD PRIMARY KEY (`role_id`);
+
+--
+-- Indexes for table `role_permission`
+--
+ALTER TABLE `role_permission`
+  ADD PRIMARY KEY (`role_id`,`permission_id`),
+  ADD KEY `permission_id` (`permission_id`);
 
 --
 -- Indexes for table `tnumber`
@@ -86,7 +163,8 @@ ALTER TABLE `tnumber`
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `role_id` (`role_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -96,13 +174,30 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `tnumber`
 --
 ALTER TABLE `tnumber`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `role_permission`
+--
+ALTER TABLE `role_permission`
+  ADD CONSTRAINT `role_permission_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `role` (`role_id`),
+  ADD CONSTRAINT `role_permission_ibfk_2` FOREIGN KEY (`permission_id`) REFERENCES `permission` (`permission_id`);
+
+--
+-- Constraints for table `user`
+--
+ALTER TABLE `user`
+  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `role` (`role_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
