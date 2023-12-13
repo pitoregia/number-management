e9ebf80163fee76321fde79966d0113632fb6860
Raihan Revi Kasya <raihanrevikasya12@gmail.com>
2023-12-13 10:20:21 +0700

    change FavIcon

--- page/user_management.php
+++ page/user_management.php
@@ -66,6 +66,11 @@ if (isset($_GET['q_user'])) {
     <meta name="description" content="">
     <meta name="author" content="">
 
+    <link rel="apple-touch-icon" sizes="180x180" href="../assets/icon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="../assets/icon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="../assets/icon/favicon-16x16.png">
+    <link rel="manifest" href="../assets/icon/site.webmanifest">
+
     <link href="../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
